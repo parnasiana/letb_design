5f678189162e7fd2730c1b4165b258eede646687
parnasiana <parnasiana@ug.uchile.cl>
2019-03-04 19:10:21 -0300

    html(index) js(main)
    menu toggle error solved

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-$('.menu-toggle').click(function() {
+// When the user clicks on <div>, open the popup
+function myFunction1() {
+  let popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+
+function myFunction2() {
+  let popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+};
+
+$(document).ready(function(){
+
+  $('.menu-toggle').on('click',function() {
   
     $('.site-nav').toggleClass('site-nav--open', 500);
-    $(this).toggleClass('open');
-    
   });
+
+})
